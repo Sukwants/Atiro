efd615e5881d07ab7cbfbafd07c26c6ec622eec8
Sukwants <sukwants547@gmail.com>
2023-08-08 23:00:58 +0800

    it's able to work

---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const { execSync, spawnSync } = require('child_process');
+const fs = require('fs');
+const chalk = require('chalk');
+
+function compileCpp() {
+  try {
+    execSync('g++ -o TEST.exe TEST.cpp', { stdio: 'inherit' });
+    console.log(chalk.green('[Compiled]'));
+  } catch (error) {
+    console.log(chalk.yellow('[Compilation Error]'));
+    process.exit(0);
+  }
+}
+
+function runTest() {
+  try {
+    const startTime = process.hrtime.bigint();
+
+    const result = spawnSync('TEST.exe', { input: fs.readFileSync('./TEST.in').toString(), timeout: 10000 });
+    fs.writeFileSync('./TEST.out', result.stdout);
+    fs.writeFileSync('./TEST.err', result.stderr);
+
+    const endTime = process.hrtime.bigint();
+
+    const runTime = Number(endTime - startTime) / 1e6;
+
+    console.log(`Return Value: ${result.status}`);
+    console.log(`Running Time: ${runTime.toFixed(0)}ms`);
+
+    if (result.error) {
+      if (result.signal) {
+        console.log(chalk.blue('[Time Limit Exceeded]'));
+      } else {
+        console.log(chalk.magenta('[Runtime Error]'));
+      }
+      process.exit(0);
+    }
+  } catch (error) {
+    console.log(chalk.gray('[Runtime Failed]'));
+    process.exit(0);
+  }
+}
+
+function compareFiles() {
+  try {
+    const content1 = fs.readFileSync('TEST.out').toString().replace(/[ \t]+$/gm, '').replace(/[\r\n]+$/, '');
+    const content2 = fs.readFileSync('TEST.ans').toString().replace(/[ \t]+$/gm, '').replace(/[\r\n]+$/, '');
+
+    if (content1 === content2) {
+      console.log(chalk.green('[Accepted]'));
+    } else {
+      console.log(chalk.red('[Wrong Answer]'));
+    }
+  } catch (error) {
+    console.log(chalk.cyan('[File Error]'));
+    process.exit(0);
+  }
+}
+
+function main() {
+  compileCpp();
+  runTest();
+  compareFiles();
+}
+
+main();
