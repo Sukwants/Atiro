1ba4ece635d1c9844a861fa1d0dcb162fb65c95b
Sukwants <95968907+Sukwants@users.noreply.github.com>
2023-08-10 17:11:54 +0800

    use commander.js to resolve parameters

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
+const { program } = require('commander');
+
 const compileCpp = require('./lib/compiler.js');
 const runTest = require('./lib/runner.js');
 const compareFiles = require('./lib/judger.js');
 
-function main() {
-  compileCpp();
-  runTest();
-  compareFiles();
-}
+program.version('1.0.0');
 
-main();
+program
+  .arguments('[file]')
+  .action((file) => {
+    compileCpp();
+    runTest();
+    compareFiles();
+  });
+
+program.parse(process.argv);
