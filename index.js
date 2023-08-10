578d12ec4528acf24e94141531196aad6771397a
Sukwants <sukwants547@gmail.com>
2023-08-10 17:31:43 +0800

    update to v0.0.4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const compileCpp = require('./lib/compiler.js');
 const runTest = require('./lib/runner.js');
 const compareFiles = require('./lib/judger.js');
 
-program.version('1.0.0');
+program.version('0.0.4');
 
 program
   .arguments('[file]')
