cb9400929253ef8c6b22e322139d449f1868cf47
Sukwants <95968907+Sukwants@users.noreply.github.com>
2023-08-14 22:53:47 +0800

    feature: set the configurations

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const chalk = require('chalk');
 
-const compileCpp = require('./lib/compiler.js');
-const runTest = require('./lib/runner.js');
-const compareFiles = require('./lib/judger.js');
+const compile = require('./lib/compiler.js');
+const run = require('./lib/runner.js');
+const judge = require('./lib/judger.js');
+const config = require('./lib/config.js');
+
+config.loadConfig();
 
 program
   .name('atiro')
@@ -19,9 +23,35 @@ program
   .option('-t, --time <time>', 'specify time limit')
   .option('-j, --judg <judg>', 'specify judger mode')
   .action((file, data, options) => {
-    compileCpp(file || 'TEST', options);
-    runTest(file || 'TEST', data || file || 'TEST', options);
-    compareFiles(data || file || 'TEST', options);
+    file = file || config.getConfigValue('file', 'name');
+    data = data || file;
+    compile(file, options);
+    run(file, data, options);
+    judge(data, options);
   });
+
+program
+  .command('config').alias('c')
+  .description('set the configurations')
+  .arguments('<key>')
+  .option('-g, --get', 'get the config value')
+  .option('-s, --set <value>', 'set the config value')
+  .option('-u, --unset', 'unset the config value')
+  .action((key, options) => {
+    key = key.split('.');
+    let type = key[0];
+    key = key[1];
+    if (!type || !key || !config.checkConfigKey(type, key)) {
+      console.log(chalk.red('[Error]'), `No config key ${type}.${key} found.`);
+    } else {
+      if (options.get) {
+        console.log(`${type}.${key}: ${config.getConfigValueTruly(type, key)}`);
+      } else if (options.set) {
+        config.setConfigValue(type, key, options.set);
+      } else if (options.unset) {
+        config.unsetConfigValue(type, key);
+      }
+    }
+  })
 
 program.parse(process.argv);
