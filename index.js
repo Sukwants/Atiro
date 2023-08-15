ebb91f4e978abea6e1ba168ae93d29e0f567a872
Sukwants <95968907+Sukwants@users.noreply.github.com>
2023-08-15 14:03:35 +0800

    change: seperate functions from index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const chalk = require('chalk');
 
-const compile = require('./lib/compiler.js');
-const run = require('./lib/runner.js');
-const judge = require('./lib/judger.js');
+const judger = require('./lib/judger.js');
 const config = require('./lib/config.js');
 
 config.loadConfig();
@@ -22,13 +19,7 @@ program
   .option('-c, --comp <comp>', 'specify compilation options')
   .option('-t, --time <time>', 'specify time limit')
   .option('-j, --judg <judg>', 'specify judger mode')
-  .action((file, data, options) => {
-    file = file || config.getConfigValue('file', 'name');
-    data = data || file;
-    compile(file, options);
-    run(file, data, options);
-    judge(data, options);
-  });
+  .action(judger);
 
 program
   .command('config').alias('c')
@@ -37,23 +28,6 @@ program
   .option('-g, --get', 'get the config value')
   .option('-s, --set <value>', 'set the config value')
   .option('-u, --unset', 'unset the config value')
-  .action((key, options) => {
-    key = key.split('.');
-    let type = key[0];
-    key = key[1];
-    if (!type || !key || !config.checkConfigKey(type, key)) {
-      console.log(chalk.red('[Error]'), `No config key ${type}.${key} found.`);
-    } else {
-      if (options.get) {
-        console.log(`${type}.${key}: ${config.getConfigValueTruly(type, key)}`);
-      } else if (options.set) {
-        config.setConfigValue(type, key, options.set);
-      } else if (options.unset) {
-        config.unsetConfigValue(type, key);
-      } else {
-        console.log(chalk.yellow('[Warning]'), 'No operation specified.');
-      }
-    }
-  })
+  .action(config.operateConfig)
 
 program.parse(process.argv);
